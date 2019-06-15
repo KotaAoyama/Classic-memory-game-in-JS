7465efbf1eadceeee35c3192c161f9a43193e65f
aoyamakota <hongcha811@gmail.com>
2019-06-15 22:05:32 +0900

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,20 +1,44 @@
-let cards = [
-    'fa-diamond', 'fa-diamond',
-    'fa-bomb', 'fa-bomb',
-    'fa-paper-plane-o', 'fa-paper-plane-o',
-    'fa-anchor', 'fa-anchor',
-    'fa-cube', 'fa-cube',
-    'fa-leaf', 'fa-leaf',
-    'fa-bicycle', 'fa-bicycle',
-    'fa-bolt', 'fa-bolt',
-];
+// Cards
+let icons = [ 'fa-diamond','fa-bomb','fa-paper-plane-o','fa-anchor','fa-cube','fa-leaf','fa-bicycle','fa-bolt'];
+let cards = [...icons, ...icons];
+initGame();
+
+let allCards = document.querySelectorAll('.card');
+let openCards = [];
+setCardsLogic();
+
+// Timer
+const timerOutput = document.querySelector('.timer-output');
+let sec = 0;
+let min = 0;
+let timer;
+startTimer();
+
+// Moves
+const movesOutput = document.querySelector('.moves');
+let moveCount = 0;
+movesOutput.innerHTML = moveCount;
+
+// Star-Rating
+const stars = document.querySelectorAll('.fa-star');
+
+// Reset-Button
+const resetButton = document.querySelector('.restart');
+resetButton.addEventListener('click', allReset);
+
+// Modal
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close');
+const modalRestartButton = document.querySelector('.modal-restart');
+modalRestartButton.addEventListener('click', restartAndHideModal);
+closeButton.addEventListener('click', hideModal);
+window.addEventListener('click', hideModal2);
 
 
-function generateCard(card){
-    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
-}
 
-
+//
+// Cards functions
+//
 function initGame() {
     let deck = document.querySelector('.deck');
     let cardHTML = shuffle(cards).map(function (card) {
@@ -23,20 +47,9 @@ function initGame() {
     deck.innerHTML = cardHTML.join('');
 }
 
-
-initGame();
-
-
-const resetButton = document.querySelector('.restart');
-resetButton.addEventListener('click', allReset);
-
-function allReset() {
-    restartTimer();
-    resetStarRating();
-    resetMoveCount();
-    resetCards();
+function generateCard(card){
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
-
 
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
@@ -52,79 +65,49 @@ function shuffle(array) {
     return array;
 }
 
+function setCardsLogic(){
+    allCards.forEach(function (card) {
+        card.addEventListener('click', function () {
+            if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+                openCards.push(card);
+                card.classList.add('open', 'show');
+                moveCount += 1;
+                movesOutput.innerHTML = moveCount;
+                starRating(moveCount);
 
-let allCards = document.querySelectorAll('.card');
-let openCards = [];
-let moveCount = 0;
-let movesOutput = document.querySelector('.moves');
-movesOutput.innerHTML = moveCount;
-const stars = document.querySelectorAll('.fa-star');
+                // two cards opened
+                if (openCards.length == 2) {
 
-// the modal
-const modal = document.getElementById('myModal');
-const closeButton = document.querySelector('.close');
-
-
-
-
-
-
-
-allCards.forEach(function (card) {
-    card.addEventListener('click', function () {
-
-        if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-            openCards.push(card);
-            card.classList.add('open', 'show');
-            moveCount += 1;
-            movesOutput.innerHTML = moveCount;
-            // StarRating
-            starRating(moveCount);
-            
-            if (openCards.length == 2) {
-                // the two cards match
-                if (openCards[0].dataset.card == openCards[1].dataset.card){
-                    openCards[0].classList.add('match');
-                    openCards[1].classList.add('match');
-                    openCards = [];
-                    
-
-                    // all cards match
-                    if (document.querySelectorAll('.match').length == cards.length){
-                        stopTimer();
-                        const timeResult = document.querySelector('.timeResult');
-                        const starsResult = document.querySelector('.starsResult');
-                        const starsOutput = document.querySelector('.stars');
-                        const movesResult = document.querySelector('.movesResult');
-                        timeResult.innerHTML = timerOutput.innerHTML;
-                        starsResult.innerHTML = starsOutput.innerHTML;
-                        movesResult.innerHTML = movesOutput.innerHTML;
-
-                        showMo();
-                    }
-                }else{
-                    // the two cards don't match
-                    setTimeout(function () {
-                        openCards.forEach(function (openCard) {
-                            openCard.classList.remove('open', 'show');
-                        });
+                    // the two cards match
+                    if (openCards[0].dataset.card == openCards[1].dataset.card){
+                        openCards[0].classList.add('match');
+                        openCards[1].classList.add('match');
                         openCards = [];
-                    }, 1000);
+
+                        // all cards match
+                        if (document.querySelectorAll('.match').length == cards.length){
+                            stopTimer();
+                            showModal();
+                        }
+                    }else{
+                        // the two cards don't match
+                        setTimeout(function () {
+                            openCards.forEach(function (openCard) {
+                                openCard.classList.remove('open', 'show');
+                            });
+                            openCards = [];
+                        }, 1000);
+                    }
                 }
             }
-        }
+        });
     });
-});
+}
 
 
-// Timer
-const timerOutput = document.querySelector('.timer-output');
-let sec = 0;
-let min = 0;
-let timer;
-
-startTimer();
-
+//
+// Timer functions
+//
 function startTimer() {
     timer = setInterval(insertTime, 1000);
 }
@@ -161,47 +144,75 @@ function restartTimer() {
 }
 
 
+//
+// Star-Rating functions
+//
 function starRating(moveCount) {
     if (moveCount > 24){
-        stars[0].style.cssText = "visibility: hidden";
+        stars[0].classList.replace('fa-star', 'fa-star-o');
     }
-    if (moveCount > 32){
-        stars[1].style.cssText = "visibility: hidden";
+    if (moveCount > 32) {
+        stars[1].classList.replace('fa-star', 'fa-star-o');
     }
 }
-
 
 function resetStarRating() {
-    stars[0].style.cssText = "";
-    stars[1].style.cssText = "";
+    stars[0].classList.replace('fa-star-o', 'fa-star');
+    stars[1].classList.replace('fa-star-o', 'fa-star');
 }
 
 
+//
+// Moves function
+//
 function resetMoveCount() {
     moveCount = 0;
     movesOutput.innerHTML = moveCount;
 }
 
 
+//
+// Reset-Button function
+//
 function resetCards() {
     allCards.forEach(function (card) {
         card.classList.remove('open', 'show', 'match');
     })
 }
 
-
-function showMo() {
-  modal.style.display = "block";
+function allReset() {
+    restartTimer();
+    resetStarRating();
+    resetMoveCount();
+    resetCards();
+    openCards = [];
 }
 
 
-closeButton.onclick = function() {
-  modal.style.display = "none";
+//
+// Modal functions
+//
+function showModal() {
+    const timeResult = document.querySelector('.timeResult');
+    const starsResult = document.querySelector('.starsResult');
+    const movesResult = document.querySelector('.movesResult');
+    timeResult.innerHTML = timerOutput.innerHTML;
+    starsResult.innerHTML = document.getElementsByClassName('fa-star').length.toString();
+    movesResult.innerHTML = movesOutput.innerHTML;
+    modal.style.display = "block";
 }
 
+function restartAndHideModal(){
+    allReset();
+    hideModal();
+}
 
-window.onclick = function(e) {
-  if (e.target == modal) {
+function hideModal() {
     modal.style.display = "none";
-  }
+}
+
+function hideModal2(e) {
+    if (e.target == modal){
+        hideModal();
+    }
 }
